67e0c03d328b6357298c39a4f01b84da42905ba7
Pablo Fernandez <fernandezpablo85@gmail.com>
2010-04-23 16:43:19 -0300

    Refactored Specs. They are easier to execute now

--- src/test/java/org/scribe/specs/Spec.java
+++ src/test/java/org/scribe/specs/Spec.java
@@ -9,7 +9,7 @@ public abstract class Spec {
 
   private static final String EQUALIZER_RESOURCES_ROOT = "src/main/resources/org/scribe/eq/";
   
-  Scribe createFrom(String properties){
+  static Scribe createFrom(String properties){
     try{
       Properties props = new Properties();
       FileInputStream fis = new FileInputStream(EQUALIZER_RESOURCES_ROOT + properties);
@@ -18,5 +18,9 @@ public abstract class Spec {
     }catch(IOException ioe){
       throw new RuntimeException("Error while creating the Scribe instance", ioe);
     }
+  }
+  
+  static void br() {
+    System.out.println();
   }
 }
--- src/test/java/org/scribe/specs/YahooSpec.java
+++ src/test/java/org/scribe/specs/YahooSpec.java
@@ -12,68 +12,62 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-*/
+ */
 package org.scribe.specs;
 
-import org.junit.*;
+import java.util.*;
+
 import org.scribe.http.*;
 import org.scribe.http.Request.*;
 import org.scribe.oauth.*;
 
-public class YahooSpec extends Spec{
+public class YahooSpec extends Spec {
 
-  private static final String YAHOO_PROPERTIES = "yahoo.properties";
+  private static final String NETWORK_NAME = "Yahoo";
+  private static final String PROPERTIES_FILE = "yahoo.properties";
   private static final String AUTHORIZE_URL = "https://api.login.yahoo.com/oauth/v2/request_auth?oauth_token=";
   private static final String PROTECTED_RESOURCE_URL = "http://social.yahooapis.com/v1/user/A6ROU63MXWDCW3Y5MGCYWVHDJI/profile/status?format=json";
-  
-  Scribe scribe;
-  
-  @Before
-  public void setup(){
-    scribe = createFrom(YAHOO_PROPERTIES);
-  }
-  
-  @Test @Ignore
-  public void getRequestToken(){
-    
-    Token requestToken = scribe.getRequestToken();
-    
-    System.out.println("Got the Request Token:");
-    System.out.println(requestToken);
-    System.out.println("Go and authorize it here:");
-    System.out.println(AUTHORIZE_URL + requestToken.getToken());
-  }
-  
-  @Test @Ignore
-  public void getAccessToken(){
-    
-    //You got this token in the previous step.
-    Token requestToken = new Token("token", "secret");
-    
-    //You got the verifier after authenticating the request token.
-    String verifier = "verifier";
-    Token accessToken = scribe.getAccessToken(requestToken, verifier);
-    
-    System.out.println("You've exchanged the request token for an access token:");
-    System.out.println(accessToken);
-    System.out.println("Also for Yahoo, make sure to copy the user id that comes in the parameter 'xoauth_yahoo_guid':");
-    System.out.println(accessToken.getRawString());
-  }
-  
-  @Test @Ignore
-  public void makeSignedCall(){
 
-    //The protected resource.
+  Scribe scribe;
+
+  public static void main(String[] args) {
+
+    Scribe scribe = createFrom(PROPERTIES_FILE);
+    Scanner in = new Scanner(System.in);
+
+    System.out.println("=== " + NETWORK_NAME + "'s OAuth Workflow ===");
+    br();
+
+    // Obtain the Request Token
+    System.out.println("Fetching the Request Token...");
+    Token requestToken = scribe.getRequestToken();
+    System.out.println("Got the Request Token!");
+    br();
+
+    System.out.println("Now go and authorize Scribe here:");
+    System.out.println(AUTHORIZE_URL + requestToken.getToken());
+    System.out.println("And paste the verifier here");
+    System.out.print(">>");
+    String verifier = in.nextLine();
+    br();
+
+    // Trade the Request Token and Verfier for the Access Token
+    System.out.println("Trading the Request Token for an Access Token...");
+    Token accessToken = scribe.getAccessToken(requestToken, verifier);
+    System.out.println("Got the Access Token!");
+    System.out.println("(if your curious it looks like this: " + accessToken + " )");
+    br();
+
+    // Now let's go and ask for a protected resource!
+    System.out.println("Now we're going to access a protected resource...");
     Request request = new Request(Verb.GET, PROTECTED_RESOURCE_URL);
-    
-    //You got this token in the previous step.
-    Token accessToken = new Token("token", "secret");
-    
-    //Sign and send the request.
     scribe.signRequest(request, accessToken);
     Response response = request.send();
-    
-    System.out.println("Request executed, got:");
+    System.out.println("Got it! Lets see what we found...");
+    br();
     System.out.println(response.getBody());
+
+    br();
+    System.out.println("Thats it man! Go and build something awesome with Scribe! :)");
   }
 }
